7628aa5d8ed8e12b7a3b4eed7fa8be7bef522d1b
Raymond Lemon <Rlemon285@gmail.com>
2015-08-12 21:46:10 -0400

    Covers the registerMultiTask function and how to pass in tasks that you can iterate through or individually run

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,28 @@
 module.exports = function (grunt) {
+/*
     grunt.initConfig(grunt.file.readJSON("config.json"))//you can read from a json file or pass in an object to grunt,
     // the properties of these object may then be read into other tasks
 
-    //{
-    //person: {
-    //    firstName: "Raymond",
-    //        lastName: "Lemon"
-    //}
-    //}
+    {
+    person: {
+        firstName: "Raymond",
+            lastName: "Lemon"
+    }
+    }
 
     grunt.registerTask("default", function (name) {//you can also pass in parameters to grunt tasks
         grunt.log.writeln("Hello, "+grunt.config.get("person").firstName +" "+grunt.config.get("person").lastName);
+    })
+*/
+    grunt.initConfig({
+        tasks: {//create an object called task with the nested object one, two etc. we can run this with the cmd grunt tasks
+            one: "first",//grunt tasks:one
+            two: "second",//grunt tasks:two
+            three: "third",//grunt tasks:three
+        }
+    })
+    //Registering multitasks allows you to either iterate through or choose just an individual task to run from a list.
+    grunt.registerMultiTask("tasks", function(){
+        grunt.log.writeln("target: "+this.target +", data: "+this.data);//prints the object name and data
     })
 }
